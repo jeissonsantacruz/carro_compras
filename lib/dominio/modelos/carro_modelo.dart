18347d123ea470e23b43883209d09e3cff84b749
Jean Carlos Santacruz <jeancarlos@javerianacali.edu.co>
2021-06-22 15:16:04 -0500

    Documentacion modelos

---
+++ lib/dominio/modelos/carro_modelo.dart
@@ -0,0 +1,13 @@
+
+/* Esta clase contiene el modelo del carrito*/
+
+class Carrito {
+    Carrito({
+       this.nombre, this.cantidad, this.imagen, this.id
+    });
+    String id;
+    String nombre;
+    int cantidad;
+    String imagen;
+   
+}
--- lib/dominio/modelos/productos_modelo.dart
+++ lib/dominio/modelos/productos_modelo.dart
@@ -1,12 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/* Esta clase contiene el modelo del producto directamente de firebase */
 class Productos {
-  final String nombre, sku, descripcion, id;
+  final String nombre, sku, descripcion, precio, imagen, id;
 
-  const Productos(this.nombre, this.sku, this.descripcion, this.id);
+  const Productos(this.nombre, this.sku, this.descripcion, this.precio,
+      this.imagen, this.id);
 
   static Productos fromSnapshot(DocumentSnapshot snapshot) {
-    return Productos(snapshot.data['nombre'], snapshot.data['sku'],
-        snapshot.data['descripcion'], snapshot.documentID);
+    return Productos(
+        snapshot.data['nombre'],
+        snapshot.data['sku'],
+        snapshot.data['descripcion'],
+        snapshot.data['precio'],
+        snapshot.data['imagen'],
+        snapshot.documentID);
   }
 }
